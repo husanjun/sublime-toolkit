58e05b9d46ae9cf6b72e18d89a3c4c10566c53e3
hujunjun <1466472672@qq.com>
2022-06-09 17:05:07 +0800

    Update code

--- auto_fold_code.py
+++ auto_fold_code.py
@@ -6,6 +6,3 @@ import os
 class AutoFoldCode(sublime_plugin.EventListener):
     def on_load_async(self, view):
         view.run_command("fold_by_level", {"level": 2})
-
-    def on_reload_async(self, view):
-        view.run_command("fold_by_level", {"level": 2})

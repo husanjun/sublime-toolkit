7412b6ad93d13dd6358f65d2f8d3fcd65778cff7
hujunjun <1466472672@qq.com>
2022-06-09 16:56:40 +0800

    Add

---
+++ auto_fold_code.py
@@ -0,0 +1,8 @@
+import sublime
+import sublime_plugin
+import os
+
+
+class AutoFoldCode(sublime_plugin.EventListener):
+    def on_load_async(self, view):
+        view.run_command("fold_by_level", {"level": 1})

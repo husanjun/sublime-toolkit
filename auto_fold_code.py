e6313e843a84cea3c49fb129e922eba2997bba54
hujunjun <1466472672@qq.com>
2022-08-03 13:58:23 +0800

    Update code

--- auto_fold_code.py
+++ auto_fold_code.py
@@ -3,7 +3,14 @@ import sublime_plugin
 import os
 
 
+class AutoFoldCodeCommand(sublime_plugin.TextCommand):
+    def run(self, edit):
+        _row, _col = self.view.rowcol(self.view.sel()[0].b)
+        self.view.run_command("fold_by_level", {"level": 1 if _row > 0 else 2})
+        # sublime.active_window().active_view().unfold(sublime.active_window().active_view().folded_regions()[2])
+
+
 class AutoFoldCode(sublime_plugin.EventListener):
     def on_load_async(self, view):
-        _row, _col = view.rowcol(view.sel()[0].b)
-        view.run_command("fold_by_level", {"level": 1 if _row > 0 else 2})
+        # view.run_command("auto_fold_code")
+        pass
